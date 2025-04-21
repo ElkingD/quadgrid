27c948a0ec394fdbb379abd4bc90a1bd1ab02f99
Dennis Elking <delking@fielddyne.com>
2025-04-21 12:27:46 -0500

    Add Google verification meta tag to Doxygen docs

---
+++ docs/html/namespaces.js
@@ -0,0 +1,4 @@
+var namespaces =
+[
+    [ "quadgrid", "namespacequadgrid.html", null ]
+];
---
+++ docs/html/unit__sphere__grid__lebedev_8hpp.js
@@ -0,0 +1,7 @@
+var unit__sphere__grid__lebedev_8hpp =
+[
+    [ "unitSphereLebedev", "unit__sphere__grid__lebedev_8hpp.html#a6266939755ae280591bc62afa6081fdc", null ],
+    [ "unitSphereLebedevLmax", "unit__sphere__grid__lebedev_8hpp.html#a4153d14cc3e2cb3387777e040a93c6f8", null ],
+    [ "unitSphereLebedevNumGrid", "unit__sphere__grid__lebedev_8hpp.html#a4582b5f0edf3a8dfe2de68b03138acaa", null ],
+    [ "unitSphereLebedevNumPoint", "unit__sphere__grid__lebedev_8hpp.html#aead2b58f6f9d4e7e627e968ce6a55f5b", null ]
+];
